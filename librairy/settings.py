6e2ef29d1485ed10ccd35b7c1536ec5859fa4534
kokou-stm <kokou.sekpona@uphf.fr>
2024-12-07 13:30:35 +0100

    add template

--- librairy/settings.py
+++ librairy/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+
 import os
+import psycopg
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 '''from dotenv import load_dotenv
@@ -81,8 +83,8 @@ WSGI_APPLICATION = 'librairy.wsgi.application'
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
-'''
+}'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
