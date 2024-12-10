302c26baaffa3f540860df5c638e360328094757
kokoustm <sitsopekokou@gmail.com>
2024-12-10 10:18:31 +0100

    change templates

--- gestion/views.py
+++ gestion/views.py
@@ -17,7 +17,9 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-    return render(request, "index.html")
+    books = Book.objects.all()
+    print(books)
+    return render(request, "index.html", {"books":books})
 
 @login_required
 def meslivres(request):
